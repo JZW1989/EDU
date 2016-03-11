0218359b1fe5e0d45d3da549c65396cd55cf0780
JZW1989 <bb413755881@126.com>
2016-03-11 22:02:05 +0800

    tab切换和课程列表的初始化
    待封装，依旧未组件化，摸索。。。。

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 
 
 /*轮播器模块*/
+//思考如何组件化？
 (function(){
 
 	var slider=document.querySelector('.m-slider');
@@ -70,6 +71,7 @@
 	//鼠标点击下标直接切换图片
 	addEvent(cursor,'click',function(e){
 		var target=e.target;
+		if(target.className==='selected')return;
 		for(var i=0;i<len;i++){
 			if(target===cursor.children[i]){
 				pageIndex=i;
@@ -79,6 +81,90 @@
 	})
 
 })();
+
+
+/*动态获取课程*/
+var Coures = (function(){
+
+	var template = 
+		'<div class="cor-item">\
+			<img>\
+			<span class="cor-title"></span>\
+			<span class="cor-provider"></span>\
+			<span class="cor-num"><strong></strong></span>\
+			<span class="cor-price"></span>\
+		</div>'
+
+	function getCoures(obj){
+
+		var couresObj = JSON.parse(obj).list;
+		var container = document.querySelector('.cor-lists');
+		var layout = html2node(template);
+
+
+		for(var i=0,len=couresObj.length;i<len;i++){
+
+			var item = layout.cloneNode(true);
+
+			var img = item.getElementsByTagName('img')[0];
+
+			var itemTitle = item.querySelector('.cor-title');
+
+			var itemPro = item.querySelector('.cor-provider');
+
+			var itemNum = item.querySelector('.cor-num').getElementsByTagName('strong')[0];
+
+			var itemPrice=item.querySelector('.cor-price');
+
+			container.appendChild(item);
+
+			img.setAttribute('src',couresObj[i].bigPhotoUrl);
+
+			itemTitle.innerText = couresObj[i].name;
+
+			itemPro.innerText = couresObj[i].provider;
+
+			itemNum.innerText = couresObj[i].learnerCount;
+
+			itemPrice.innerText = '￥'+couresObj[i].price;
+
+		}
+	}
+
+	return {
+
+		getCoures : getCoures
+
+	}
+
+})();
+
+//初始化
+ajax_get('http://study.163.com/webDev/couresByCategory.htm',
+		{'pageNo':1,'psize':20,'type':10},Coures.getCoures);
+
+
+//切换
+var tab=document.querySelector('.m-cor .tab');
+tab.onclick=function(e){
+	var target=e.target;
+	if(target.className === 'bt active')return;
+	var container = document.querySelector('.cor-lists');
+	var len=target.parentNode.children.length;
+	for(var i=0;i<len;i++){
+		target.parentNode.children[i].className='bt';
+	}
+	target.className='bt active';
+	container.innerHTML='';
+	if(target.value==="产品设计"){
+		ajax_get('http://study.163.com/webDev/couresByCategory.htm',
+		{'pageNo':1,'psize':20,'type':10},Coures.getCoures);
+	}else if(target.value==="编程语言"){
+		ajax_get('http://study.163.com/webDev/couresByCategory.htm',
+		{'pageNo':1,'psize':20,'type':20},Coures.getCoures);
+	}
+}
+
 
 
 
--- js/util.js
+++ js/util.js
@@ -1,4 +1,102 @@
+
+//添加事件
 function addEvent(ele,type,fn){
   if(ele.addEventListener)return ele.addEventListener(type,fn);
   else return ele.attachEvent('on'+type,fn);
 }
+
+//ajax的GET方法
+function ajax_get(url,opt,callback){
+    var xhr=new XMLHttpRequest();
+
+    var opt=serialize(opt);
+    url=url+'?'+opt;
+    xhr.open('GET',url,true);
+    xhr.send(null);
+
+    xhr.onload=function(){
+      callback(xhr.responseText);
+    }
+  }
+
+
+
+//序列化参数
+function serialize(obj){
+  var arr=[];
+  for(var i in obj){
+    if(!obj.hasOwnProperty(i)) continue;
+        if(typeof obj[i]==='function') continue;
+    arr.push(encodeURIComponent(i)+'='+encodeURIComponent(obj[i]));
+  }
+  return arr.join('&');
+}
+
+function html2node(str){
+    var container = document.createElement('div');
+    container.innerHTML = str;
+    return container.children[0];
+  }
+
+var emitter = {
+  // 注册事件
+  on: function(event, fn) {
+    var handles = this._handles || (this._handles = {}),
+      calls = handles[event] || (handles[event] = []);
+
+    // 找到对应名字的栈
+    calls.push(fn);
+
+    return this;
+  },
+  // 解绑事件
+  off: function(event, fn) {
+    if(!event || !this._handles) this._handles = {};
+    if(!this._handles) return;
+
+    var handles = this._handles , calls;
+
+    if (calls = handles[event]) {
+      if (!fn) {
+        handles[event] = [];
+        return this;
+      }
+      // 找到栈内对应listener 并移除
+      for (var i = 0, len = calls.length; i < len; i++) {
+        if (fn === calls[i]) {
+          calls.splice(i, 1);
+          return this;
+        }
+      }
+    }
+    return this;
+  },
+  // 触发事件
+  emit: function(event){
+    var args = [].slice.call(arguments, 1),
+      handles = this._handles, calls;
+
+    if (!handles || !(calls = handles[event])) return this;
+    // 触发所有对应名字的listeners
+    for (var i = 0, len = calls.length; i < len; i++) {
+      calls[i].apply(this, args)
+    }
+    return this;
+  }
+}
+
+
+function html2node(str){
+    var container = document.createElement('div');
+    container.innerHTML = str;
+    return container.children[0];
+  }
+
+  // 赋值属性
+  // extend({a:1}, {b:1, a:2}) -> {a:1, b:1}
+  function extend(o1, o2){
+    for(var i in o2) if(typeof o1[i] === 'undefined'){
+      o1[i] = o2[i]
+    } 
+    return o1
+  }
